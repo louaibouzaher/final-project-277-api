f675dff74ad7a75ccf251bee28d6f06dc91e5b52
louaibouzaher <jm_bouzaher@esi.dz>
2022-05-02 02:52:02 +0300

    Added create and edit league

--- app/controllers/LeaguesControllers.js
+++ app/controllers/LeaguesControllers.js
@@ -13,6 +13,45 @@ exports.getAllLeagues = async (req, res) => {
     res.send(err);
   }
 };
+
+exports.createLeague = async (req, res) => {
+  try {
+    const league = req.body;
+    await db.query(
+      `INSERT INTO league (leagueName,leagueType,logo)
+    VALUES ("${league.leagueName}", "${league.leagueType}", "${league.logo}");`,
+      (error, results, fields) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+          return;
+        }
+        res.send(results);
+      }
+    );
+  } catch (err) {
+    res.send(err);
+  }
+};
+exports.editLeague = async (req, res) => {
+  try {
+    const league = req.body;
+    await db.query(
+      `UPDATE league set leagueName="${league.leagueName}" , leagueType ="${league.leagueType}", logo= "${league.logo}" where Id = ${league.Id};`,
+      (error, results, fields) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+          return;
+        }
+        res.send(results);
+      }
+    );
+  } catch (err) {
+    res.send(err);
+  }
+};
+
 exports.getLeagueMatches = async (req, res) => {
   try {
     await db.query(
@@ -32,7 +71,6 @@ exports.getLeagueMatches = async (req, res) => {
 };
 exports.getLeagueChampions = async (req, res) => {
   try {
-    console.log(req.params.id);
     await db.query(
       `select * from viewchampion vc join league l on vc.leagueName = l.leagueName where l.Id = ${req.params.id}`,
       (error, results, fields) => {
@@ -79,6 +117,25 @@ exports.addMatch = async (req, res) => {
       }
       res.send(results);
     });
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.deleteLeague = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    await db.query(
+      `delete from league where Id = ${req.params.id}`,
+      (error, results, fields) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+          return;
+        }
+        res.send(results);
+      }
+    );
   } catch (err) {
     res.send(err);
   }
--- app/routes/LeaguesRoutes.js
+++ app/routes/LeaguesRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const LeaguesControllers = require("../controllers/LeaguesControllers");
 
 router.get("/all", LeaguesControllers.getAllLeagues);
+router.post("/create", LeaguesControllers.createLeague);
 router.get("/matches/:id", LeaguesControllers.getLeagueMatches);
 router.get("/champions/:id", LeaguesControllers.getLeagueChampions);
 router.post("/addchampion", LeaguesControllers.addChampion);
 router.post("/addmatch", LeaguesControllers.addMatch);
+router.put("/update/:id", LeaguesControllers.editLeague);
+router.delete("/delete/:id", LeaguesControllers.deleteLeague);
 
 module.exports = router;
